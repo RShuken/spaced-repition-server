f5af29676a36262f7298f0fe90dbb13e88a069eb
rshuken <ryanshuken@gmail.com>
2021-01-10 16:15:25 -0700

    more linkedlist

--- src/linkedList.js
+++ src/linkedList.js
@@ -4,3 +4,39 @@ class _Node {
     this.next = next;
   }
 }
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(item) {
+        this.head = new _Node(item, this.head);
+    }
+
+    insertLast(item) {
+        if (this.head === null) {
+            this.insertFirst(item);
+        } else {
+            let tempNode = this.head;
+            while (tempNode.next !== null) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = new _Node(item, null);
+        }
+    }
+
+    find(item) {
+        let currNode = this.head;
+        if (!currNode) {
+            return null;
+        }
+        while (currNode.value !== item) {
+            if (currNode.next === null) {
+                return null;
+            } else {
+                currNode = currNode.next;
+            }
+        }
+        return currNode;
+    }
