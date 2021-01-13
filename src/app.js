3de5a21452716eb09532e442b7d80c2ea259d884
rshuken <ryanshuken@gmail.com>
2021-01-13 15:18:29 -0700

    depolyment v1

--- src/app.js
+++ src/app.js
@@ -13,18 +13,20 @@ const app = express()
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
   skip: () => NODE_ENV === 'test',
 }))
-app.use(cors())
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
+}))
 app.use(helmet())
 
-app.get('/', (req, res) => {
-  res.send('Hello World! This public route works')
-});
+
 
 app.use('/api/auth', authRouter)
 app.use('/api/language', languageRouter)
 app.use('/api/user', userRouter)
 
-
+app.get('/', (req, res) => {
+  res.send('Hello World! This public route works')
+});
 
 
 app.use(errorHandler)
